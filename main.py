323f47f7ea149fb46ca8d8280af88fc4d78332b0
Aetherlyn <ahmetaydincapar@gmail.com>
2025-12-26 17:50:01 +0300

    CH:2 L:4

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
@@ -14,6 +17,9 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+          
     
 
 
